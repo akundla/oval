5bb08b4c82c534fea49cacd95ed58fb3a8db6ee9
akundla <alekkundla@gmail.com>
2021-03-06 14:29:09 -0500

    Changed type of serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,17 +2,17 @@ from rest_framework import serializers
 from .models import User, Role, Enrolled, Class, Term, Tag, Comment, Answer, Post
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
 
-class RoleSerializer(serializers.HyperlinkedModelSerializer):
+class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = ['name']
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['created_date', 'modified_date', 'title', 'body', 'answerable', 'tags', 'upvotes', 'author', 'comments', 'answers']
