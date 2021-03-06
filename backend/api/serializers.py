f2d0f6fe65380b7bcbff3323194fdcd18dc08046
akundla <alekkundla@gmail.com>
2021-03-06 01:10:52 -0500

    Created a serializer for user created a 'view', which will hopefully end up as  just a collection of API methods

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from .models import User, Role, Enrolled, Class, Term, Tag, Comment, Answer, Post
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['first_name', 'last_name', 'email']
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from .serializers import UserSerializer
+from rest_framework import viewsets
+from .models import User, Role, Enrolled, Class, Term, Tag, Comment, Answer, Post
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('last_name')
+    serializer_class = UserSerializer
