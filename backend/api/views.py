5491cfab7b7eb3f7c484f9222ddc234096934e34
maxdg99 <maxdg99@gmail.com>
2021-03-06 02:16:43 -0500

    Breaking out some of the necessary options

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,49 @@
 
 from .serializers import UserSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .models import User, Role, Enrolled, Class, Term, Tag, Comment, Answer, Post
 
-
+# User Routes
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('last_name')
     serializer_class = UserSerializer
+
+# Needed Routes
+# Sign Up
+# Login
+
+# View User
+# Edit User
+
+# Create Class
+# View Class (statistics)
+# Edit Class (for primary instructor only)
+
+# Create Post
+# View Posts (paginated, filered)
+# View Single Post
+# Edit single post (author only)
+
+# Up Vote
+# Undo Up Vote
+# View up votes
+
+# Create Tag (primary instructor only)
+# Edit Tags (primary instructor only)
+# View Tags
+
+# Add Comment
+# View Comments
+# Edit Comment (author only)
+
+# Add Answer
+# View Answers
+# Edit Answer
+
+# Get Course Info page
+# Edit Course Info Page (primary instructor only)
+
+# Request join link
+
+# Notes
+# edit/create tags, changing course settings, and editing course info should be on a settings page
