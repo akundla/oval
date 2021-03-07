9d3e88080428b3c5b9121af89c432cbc23107e43
akundla <alekkundla@gmail.com>
2021-03-06 19:43:52 -0500

    you can add things!

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,25 +6,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['pk', 'first_name', 'last_name', 'email', 'password']
-        depth = 1
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = ['pk', 'name']
-        depth = 1
 
 class EnrolledSerializer(serializers.ModelSerializer):
     class Meta:
         model = Enrolled
         fields = ['pk', 'user_id', 'class_id', 'role', 'tag']
-        depth = 1
 
 class ClassSerializer(serializers.ModelSerializer):
     class Meta:
         model = Class
         fields = ('pk', 'name', 'description', 'information_page', 'enrolles', 'primary_instructor')
-        depth = 1
 
     def to_representation(self, instance):
         self.fields['primary_instructor'] = UserSerializer(read_only=True)
@@ -34,28 +30,23 @@ class TermSerializer(serializers.ModelSerializer):
     class Meta:
         model = Term
         fields = ['pk', 'full_name', 'code']
-        depth = 1
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = ['pk', 'name']
-        depth = 1
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['pk', 'created_date', 'modified_date', 'body', 'upvotes', 'author']
-        depth = 1
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
         fields = ['pk', 'created_date', 'modified_date', 'body', 'upvotes', 'author', 'comments']
-        depth = 1
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['pk', 'created_date', 'modified_date', 'title', 'body', 'answerable', 'tags', 'upvotes', 'author', 'comments', 'answers']
-        depth = 1
