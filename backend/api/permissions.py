f482d495e2796c0d73cfe333f904e90c9e029fc3
maxdg99 <maxdg99@gmail.com>
2021-03-06 23:46:09 -0500

    Add permissions for posts

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,3 +1,5 @@
+from rest_framework.exceptions import ValidationError
+from .models import User, Role, Enrolled, Class, Term, Tag, Comment, Answer, Post
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -10,5 +12,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         return obj.owner == request.user
 
 class IsInClass(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return None
+    def has_permission(self, request, view):
+        class_in = request.query_params.get("class_in")
+        if class_in is not None:
+            current_class = Class.objects.get(pk=class_in)
+            return request.user in current_class.enrollees.all() or request.user.pk == current_class.primary_instructor.pk
+        else:
+            raise ValidationError(detail="class_in parameter is required.")
+         
--- backend/api/views.py
+++ backend/api/views.py
@@ -18,92 +18,52 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('last_name')
     serializer_class = UserSerializer
 
+
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all().order_by('name')
     serializer_class = RoleSerializer
+
 
 class EnrolledViewSet(viewsets.ModelViewSet):
     queryset = Enrolled.objects.all().order_by('tag')
     serializer_class = EnrolledSerializer
 
+
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Class.objects.all().order_by('name')
     serializer_class = ClassSerializer
+
 
 class TermViewSet(viewsets.ModelViewSet):
     queryset = Term.objects.all().order_by('code')
     serializer_class = TermSerializer
 
+
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all().order_by('name')
     serializer_class = TagSerializer
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('created_date')
     serializer_class = CommentSerializer
 
+
 class AnswerViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsInClass]
     queryset = Answer.objects.all().order_by('created_date')
     serializer_class = AnswerSerializer
 
+
 class PostViewSet(viewsets.ModelViewSet):
-    #permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsInClass]
     serializer_class = PostSerializer
     pagination_class = PageNumberPagination
 
     def get_queryset(self):
-        class_in = self.request.query_params.get("class_in", None)
-        if class_in is not None:
-            #raise ValidationError(detail="class_in pk is a required parameter on this request.")
-            current_class = Class.objects.get(pk=class_in)
-        
+        current_class = Class.objects.get(
+            pk=self.request.query_params.get("class_in", None))
 
-            if (self.request.user not in current_class.enrollees.all()) and (self.request.user.pk != current_class.primary_instructor.pk):
-                raise ValidationError(detail="The signed in user does not have access to this class.")
+        return Post.objects.filter(class_in=current_class.pk).order_by('created_date')
 
-            queryset = Post.objects.filter(class_in=current_class.pk).order_by('created_date')
-        else:
-            queryset = Post.objects.all().order_by('created_date')
 
-        return queryset
-
-# Filters
-# Paginated results per class
-
-# Needed Routes
-
-# View User
-# Edit User
-
-# Create Class
-# View Class (statistics)
-# Edit Class (for primary instructor only)
-
-# Create Post
-# View Posts (paginated, filered)
-# View Single Post
-# Edit single post (author only)
-
-# Up Vote
-# Undo Up Vote
-# View up votes
-
-# Create Tag (primary instructor only)
-# Edit Tags (primary instructor only)
-# View Tags
-
-# Add Comment
-# View Comments
-# Edit Comment (author only)
-
-# Add Answer
-# View Answers
-# Edit Answer
-
-# Get Course Info page
-# Edit Course Info Page (primary instructor only)
-
-# Request join link
-
-# Notes
-# edit/create tags, changing course settings, and editing course info should be on a settings page
